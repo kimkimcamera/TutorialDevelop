c5c32137e0eba9a77c4e0cb63fad7e97b4da9747
kimkimcamera <kimkimcamera@gmail.com>
2024-01-23 19:26:45 +0900

    Chapter10で作成、修正したJavaクラスおよびファイルです。

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -8,6 +8,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
@@ -24,15 +32,22 @@ public class User {
     private Integer id;
     
     @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max=20)
     private String name;
     
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
     
+    @Min(0)
+    @Max(120)
     private Integer age;
     
     @Column(length = 50)
+    @Email
+    @Length(max=50)
     private String email;
     
 }
